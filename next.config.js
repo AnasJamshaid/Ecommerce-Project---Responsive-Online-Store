e39ae23e48545088ae8dfb8ee5904cce0ee638a0
Muhammad Anas Jamshaid <anassheikh54211@gmail.com>
2025-01-30 13:13:49 +0500

    upatde

--- next.config.js
+++ next.config.js
@@ -1,14 +1,9 @@
 module.exports = {
-  images: {
-    domains: ['cdn.sanity.io'], // Add this line
-  },
-  eslint: {
-    ignoreDuringBuilds: true, // Temporarily ignore ESLint errors during builds
-  },
-  webpack: (config, { dev }) => {
-    if (dev) {
-      config.devtool = 'cheap-module-source-map'; // Debugging
-    }
-    return config;
-  },
-};
+    images: {
+      domains: ['cdn.sanity.io'], // Add this line
+    },
+    eslint: {
+      ignoreDuringBuilds: true, // Temporarily ignore ESLint errors during builds
+    },
+  }
+  
