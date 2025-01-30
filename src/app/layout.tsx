1ac5d250a1f58fae4cefe5d8362f51d93db61070
Muhammad Anas Jamshaid <anassheikh54211@gmail.com>
2025-01-30 14:01:06 +0500

    uptade

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ import { ClerkProvider } from "@clerk/nextjs";
 
 
 export const metadata: Metadata = {
-  title: "Professional Food Truck Website Design to Boost Your Business",
+  title: "Food Truck Website Design to Boost Your Business",
   description: "Get a professional and visually appealing food truck website designed to showcase your brand and attract more customers. I specialize in creating modern, responsive websites tailored to the unique needs of food truck businesses.",
 };
 
